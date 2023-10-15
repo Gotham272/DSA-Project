9d11cebe602ba0dfa6c243f0952155f19aabcf40
Shashank Bakshi <146818136+Gotham272@users.noreply.github.com>
2023-10-15 21:06:45 +0530

    GUI fucnctionalityu for C

--- main.c
+++ main.c
@@ -4,15 +4,16 @@
 #include <string.h>
 #include <conio.h>
 #include <windows.h>
+#include <gtk/gtk.h>
 
 struct person{
   char name[50];
   char address[50];
-  char father_name[50];
-  int mobile_no;
-  char gender[20];
+  // char father_name[50];
+  long int mobile_no;
+  // char gender[20];
   char email[100];
-  char citizen[100]
+  // char citizen[100]
 };
 
 void menu();
@@ -27,20 +28,6 @@ void search_record();
 
 int main(void)
 {
-  // system("color #FF0000");
-  start();
-  return 0;
-}
-
-void back()
-{
-  start();
-}
-
-void start()
-{
-  menu();
-}
 
 void menu(){
   system("cls");
@@ -48,5 +35,5 @@ void menu(){
   printf("\n\n\t\t Main Menu");
   printf("\n\n Choose the Option According to ur choice");
   printf("\n\n1.Add new Contact \n2.View Contact list \n3.Edit Contact List \n4.Search Contact \n5.Delete Contact \n6.Exit My PhoneBook");
-
+}
 }
